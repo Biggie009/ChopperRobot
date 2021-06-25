90b79e1fe1ca7ef7f5ec9600eac524c81a391498
Biggie009 <85871114+Biggie009@users.noreply.github.com>
2021-06-25 22:17:57 +0530

    Update msg_types.py

--- DaisyX/utils/filters/msg_types.py
+++ DaisyX/utils/filters/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from LightYagami.modules.helper_funcs.string_handling import \
+from DaisyX.utils.filters.string_handling import \
     button_markdown_parser
 from telegram import Message
 
