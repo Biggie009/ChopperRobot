a327b0bcc2349e984f6dbb8875d693a909dc6053
Biggie009 <85871114+Biggie009@users.noreply.github.com>
2021-07-03 11:37:10 +0530

    Update pm_menu.py

--- DaisyX/modules/pm_menu.py
+++ DaisyX/modules/pm_menu.py
@@ -45,17 +45,14 @@ async def get_start_func(message, strings, edit=False):
     buttons.add(InlineKeyboardButton(strings["btn_help"], callback_data="get_help"))
     buttons.add(
         InlineKeyboardButton(strings["btn_lang"], callback_data="lang_btn"),
-        InlineKeyboardButton(
-            strings["btn_source"], url="https://github.com/TeamDaisyX/"
-        ),
-    )
+        )
     buttons.add(
         InlineKeyboardButton(" Anime Channel ", url="https://t.me/DaisyXUpdates"),
     )
     buttons.add(
         InlineKeyboardButton(
             "Add Zoro to your Group",
-            url=f"https://telegram.me/daisyxbot?startgroup=true",
+            url=f"https://telegram.me/roronoarobot?startgroup=true",
         )
     )
     # Handle error when user click the button 2 or more times simultaneously
