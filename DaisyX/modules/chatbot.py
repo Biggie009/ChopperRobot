7638fb1a4f10faeb704abcc9ebf7645c1155d18c
Biggie009 <85871114+Biggie009@users.noreply.github.com>
2021-07-04 18:42:47 +0530

    Update chatbot.py

--- DaisyX/modules/chatbot.py
+++ DaisyX/modules/chatbot.py
@@ -1,30 +1,14 @@
-# Copyright (C) 2021 Red-Aura & TeamDaisyX & HamkerCat
-
-# This file is part of Daisy (Telegram Bot)
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import re
 
 import emoji
 
-IBM_WATSON_CRED_URL = "https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/bd6b59ba-3134-4dd4-aff2-49a79641ea15"
-IBM_WATSON_CRED_PASSWORD = "UQ1MtTzZhEsMGK094klnfa-7y_4MCpJY1yhd52MXOo3Y"
 url = "https://acobot-brainshop-ai-v1.p.rapidapi.com/get"
 import re
 
 import aiohttp
-from google_trans_new import google_translator
+
+# from google_trans_new import google_translator
+from googletrans import Translator as google_translator
 from pyrogram import filters
 
 from DaisyX import BOT_ID
@@ -63,37 +47,6 @@ async def fetch(url):
 daisy_chats = []
 en_chats = []
 # AI Chat (C) 2020-2021 by @InukaAsith
-"""
-@daisyx.on_message(
-    filters.voice & filters.reply & ~filters.bot & ~filters.via_bot & ~filters.forwarded,
-    group=2,
-)
-async def hmm(client, message):
-    if not get_session(int(message.chat.id)):
-        message.continue_propagation()
-    if message.reply_to_message.from_user.id != BOT_ID:
-        message.continue_propagation()
-    previous_message = message
-    required_file_name = message.download()
-    if IBM_WATSON_CRED_URL is None or IBM_WATSON_CRED_PASSWORD is None:
-        await message.reply(
-            "You need to set the required ENV variables for this module. \nModule stopping"
-        )
-    else:
-        headers = {
-            "Content-Type": previous_message.voice.mime_type,
-        }
-        data = open(required_file_name, "rb").read()
-        response = requests.post(
-            IBM_WATSON_CRED_URL + "/v1/recognize",
-            headers=headers,
-            data=data,
-            auth=("apikey", IBM_WATSON_CRED_PASSWORD),
-        )
-        r = response.json()
-        print(r)
-        await client.send_message(message, r)
-"""
 
 
 @daisyx.on_message(
@@ -113,30 +66,28 @@ async def hmm(_, message):
         lel = await edit_or_reply(message, "`Processing...`")
         lol = add_chat(int(message.chat.id))
         if not lol:
-            await lel.edit("Chatbot successfully activated in the chat")
+            await lel.edit("Daisy AI Already Activated In This Chat")
             return
         await lel.edit(
-            f"Chatbot successfully loaded for users in the chat {message.chat.id}"
-)
-        
+            f"Daisy AI Successfully Added For Users In The Chat {message.chat.id}"
+        )
 
     elif status == "OFF" or status == "off" or status == "Off":
         lel = await edit_or_reply(message, "`Processing...`")
         Escobar = remove_chat(int(message.chat.id))
         if not Escobar:
-            await lel.edit("Zoro chatbot was not activated in the chat")
+            await lel.edit("Daisy AI Was Not Activated In This Chat")
             return
         await lel.edit(
-            f"Zoro chatbot was successfully deactivated for the users in the chat {message.chat.id}"
-)
-        
+            f"Daisy AI Successfully Deactivated For Users In The Chat {message.chat.id}"
+        )
 
     elif status == "EN" or status == "en" or status == "english":
         if not chat_id in en_chats:
             en_chats.append(chat_id)
-            await message.reply_text("Chatbot Enabled(English)")
+            await message.reply_text("English AI chat Enabled!")
             return
-        await message.reply_text("Chatbot is already disabled")
+        await message.reply_text("AI Chat Is Already Disabled.")
         message.continue_propagation()
     else:
         await message.reply_text(
@@ -170,13 +121,13 @@ async def hmm(client, message):
         message.continue_propagation()
     if chat_id in en_chats:
         test = msg
-        test = test.replace("roronoa", "Zoro")
-        test = test.replace("Roronoa", "Zoro")
+        test = test.replace("daisy", "Aco")
+        test = test.replace("Daisy", "Aco")
         response = await lunaQuery(
             test, message.from_user.id if message.from_user else 0
         )
-        response = response.replace("Zoro", "Roronoa")
-        response = response.replace("zoro", "Roronoa")
+        response = response.replace("Aco", "Daisy")
+        response = response.replace("aco", "Daisy")
 
         pro = response
         try:
@@ -216,27 +167,32 @@ async def hmm(client, message):
             # print (rm)
         try:
             lan = translator.detect(rm)
+            lan = lan.lang
         except:
             return
         test = rm
         if not "en" in lan and not lan == "":
             try:
-                test = translator.translate(test, lang_tgt="en")
+                test = translator.translate(test, dest="en")
+                test = test.text
             except:
                 return
         # test = emoji.demojize(test.strip())
 
-        test = test.replace("roronoa", "Zoro")
-        test = test.replace("Roronoa", "Zoro")
+        test = test.replace("daisy", "Aco")
+        test = test.replace("Daisy", "Aco")
         response = await lunaQuery(
             test, message.from_user.id if message.from_user else 0
         )
-        response = response.replace("Zoro", "Roronoa")
-        response = response.replace("zoro", "Roronoa")
+        response = response.replace("Aco", "Daisy")
+        response = response.replace("aco", "Daisy")
+        response = response.replace("Luna", "Daisy")
+        response = response.replace("luna", "Daisy")
         pro = response
         if not "en" in lan and not lan == "":
             try:
-                pro = translator.translate(pro, lang_tgt=lan[0])
+                pro = translator.translate(pro, dest=lan)
+                pro = pro.text
             except:
                 return
         try:
@@ -283,28 +239,31 @@ async def inuka(client, message):
         # print (rm)
     try:
         lan = translator.detect(rm)
+        lan = lan.lang
     except:
         return
     test = rm
     if not "en" in lan and not lan == "":
         try:
-            test = translator.translate(test, lang_tgt="en")
+            test = translator.translate(test, dest="en")
+            test = test.text
         except:
             return
 
     # test = emoji.demojize(test.strip())
 
     # Kang with the credits bitches @InukaASiTH
-    test = test.replace("roronoa", "Zoro")
-    test = test.replace("Roronoa", "Zoro")
+    test = test.replace("daisy", "Aco")
+    test = test.replace("Daisy", "Aco")
 
     response = await lunaQuery(test, message.from_user.id if message.from_user else 0)
-    response = response.replace("Zoro", "Roronoa")
-    response = response.replace("zoro", "Roronoa")
+    response = response.replace("Aco", "Daisy")
+    response = response.replace("aco", "Daisy")
 
     pro = response
     if not "en" in lan and not lan == "":
-        pro = translator.translate(pro, lang_tgt=lan[0])
+        pro = translator.translate(pro, dest=lan)
+        pro = pro.text
     try:
         await daisyx.send_chat_action(message.chat.id, "typing")
         await message.reply_text(pro)
@@ -313,7 +272,7 @@ async def inuka(client, message):
 
 
 @daisyx.on_message(
-    filters.regex("Zoro|zoro|Roronoa|roronoa|RoronoaZoro")
+    filters.regex("Daisy|daisy|DaisyX|daisyx|Daisyx")
     & ~filters.bot
     & ~filters.via_bot
     & ~filters.forwarded
@@ -355,27 +314,30 @@ async def inuka(client, message):
         # print (rm)
     try:
         lan = translator.detect(rm)
+        lan = lan.lang
     except:
         return
     test = rm
     if not "en" in lan and not lan == "":
         try:
-            test = translator.translate(test, lang_tgt="en")
+            test = translator.translate(test, dest="en")
+            test = test.text
         except:
             return
 
     # test = emoji.demojize(test.strip())
 
-    test = test.replace("roronoa", "Zoro")
-    test = test.replace("Roronoa", "Zoro")
+    test = test.replace("daisy", "Aco")
+    test = test.replace("Daisy", "Aco")
     response = await lunaQuery(test, message.from_user.id if message.from_user else 0)
-    response = response.replace("Zoro", "Roronoa")
-    response = response.replace("zoro", "Roronoa")
+    response = response.replace("Aco", "Daisy")
+    response = response.replace("aco", "Daisy")
 
     pro = response
     if not "en" in lan and not lan == "":
         try:
-            pro = translator.translate(pro, lang_tgt=lan[0])
+            pro = translator.translate(pro, dest=lan)
+            pro = pro.text
         except Exception:
             return
     try:
@@ -387,8 +349,7 @@ async def inuka(client, message):
 
 __help__ = """
 <b> Chatbot </b>
-CHATBOT ENABLES ZORO TO INTERACT WITH MEMBERS.
-
+DAISY AI 3.0 IS THE ONLY AI SYSTEM WHICH CAN DETECT & REPLY UPTO 200 LANGUAGES
  - /chatbot [ON/OFF]: Enables and disables AI Chat mode (EXCLUSIVE)
  - /chatbot EN : Enables English only chatbot
  
